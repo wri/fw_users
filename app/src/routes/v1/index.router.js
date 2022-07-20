5f79564ca194e273884ab53c00b3b66a64000434
Edward Martin <edward@3sidedcube.com>
2022-07-20 10:19:07 +0100

    add route

---
+++ app/src/routes/v1/index.router.js
@@ -0,0 +1,32 @@
+const logger = require("logger").default;
+const Router = require("koa-router");
+
+const router = new Router({
+  prefix: "/users"
+});
+
+class UserRouter {
+  static async delete(ctx) {
+    
+  }
+}
+
+const isAuthenticatedMiddleware = async (ctx, next) => {
+  logger.info(`Verifying if user is authenticated`);
+  const { query, body } = ctx.request;
+
+  const user = {
+    ...(query.loggedUser ? JSON.parse(query.loggedUser) : {}),
+    ...body.loggedUser
+  };
+
+  if (!user || !user.id) {
+    ctx.throw(401, "Unauthorized");
+    return;
+  }
+  await next();
+};
+
+router.delete("/", isAuthenticatedMiddleware, UserRouter.delete);
+
+export default router;
